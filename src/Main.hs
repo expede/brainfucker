5a74e35f3c43c5a24d9285964082db10d6eb212e
Brooklyn Zelenka <be.zelenka@gmail.com>
2015-03-02 22:36:09 -0800

    Renamed several datatypes; started on machine steps

--- src/Main.hs
+++ src/Main.hs
@@ -1,32 +1,54 @@
 module Main where
 
-import Data.List.Zipper
+import qualified Data.List.Zipper    as Z
+import qualified Data.Vector.Generic as V
+-- NB: Comments will distinguish between "tape" for memoryspace,
+--     and "machine" for command sequence.
 
--- Using a "tape" metaphor
-newtype Tape = [Int]
+newtype Cell    = Int
+newtype Tape    = Zipper Cell
+newtype Index   = Int
+newtype Machine = V.Vector Command
 
-data Command = MoveLeft
-             | MoveRight
-             | IncrementValue
-             | DecrementValue
-             | WriteOut
-             | ReadIn
-             | JumpForward
-             | JumpBackward
+data Command = TapeLeft
+             | TapeRight
+             | IncrementCell
+             | DecrementCell
+             | WriteFromCell
+             | ReadToCell
+             | ControlJumpRight
+             | ControlJumpLeft
 
 charToCommand :: Char -> Command
 charToCommand c = case c of
-  '<' -> MoveLeft
-  '>' -> MoveRight
-  '+' -> IncrementValue
-  '-' -> DecrementValue
-  '.' -> WriteOut
-  ',' -> ReadIn
-  '[' -> JumpForward
-  ']' -> JumpBackward
+  '<' -> TapeLeft
+  '>' -> TapeRight
+  '+' -> IncrementCell
+  '-' -> DecrementCell
+  '.' -> WriteFromCell
+  ',' -> ReadToCell
+  '[' -> ControlJumpRight
+  ']' -> ControlJumpLeft
   _   -> error "invalid character"
 
-lex :: String -> [Command]
+lex :: String -> Machine
 lex = map charToCommand
+
+operateMachine :: Tape -> Command -> Tape
+operateMachine (Zip [] []) _       = Z.insert 0 tape
+operateMachine tape        command = case command of
+  TapeLeft         -> Z.left tape
+  TapeRight        -> Z.right tape
+  IncrementCell    -> Z.replace (succ pointer) tape
+  DecrementCell    -> Z.replace (pred pointer) tape
+  WriteFromCell    -> _ -- print to $STDOUT
+  ReadToCell       -> _ -- ask for input
+  ControlJumpRight -> if pointer == 0
+                       then _ -- find the instruction AFTER the next JumpBackward
+                       else tape
+  ControlJumpLeft  -> if pointer /= 0
+                       then tape
+                       else _ -- find instruction right AFTER the last JumpForward
+  where pointer = Z.cursor tape
 
 main = putStrLn "Hello World"
