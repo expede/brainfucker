6c17cdc2b91b27c8c219da5462d4008a96c7253f
Brooklyn Zelenka <be.zelenka@gmail.com>
2015-03-05 19:03:36 -0800

    Switched to List from Vector. Compiled successfully.

--- src/Main.hs
+++ src/Main.hs
@@ -2,15 +2,16 @@ module Main where
 
 import qualified Data.List.Zipper    as Z
 import qualified Data.Vector.Generic as V
-import Data.Char (chr)
+import qualified Data.Char           as C
 
 -- Tape for "memory"
-newtype Cell = Int
-newtype Tape = Zipper Cell
+type Cell = Int
+type Tape = Z.Zipper Cell
 
 -- Machine for command sequence
-newtype Index   = Int
-newtype Machine = V.Vector Command
+type Index   = Int
+-- data Machine = Vector [Command] deriving (Show, Eq)
+type Machine = [Command]
 
 data Command = TapeLeft
              | TapeRight
@@ -21,6 +22,7 @@ data Command = TapeLeft
              | LoopStart
              | LoopEnd
              | Ignore
+             deriving (Show, Eq)
 
 charToCommand :: Char -> Command
 charToCommand c = case c of
@@ -34,8 +36,8 @@ charToCommand c = case c of
   ']' -> LoopEnd
   _   -> Ignore
 
-lex :: String -> Machine
-lex = filter (/= Ignore) $ map charToCommand
+toMachine :: String -> Machine
+toMachine = filter (/= Ignore) . map charToCommand
 
 loopStartIndex :: Index -> Machine -> Index
 loopStartIndex startIndex machine = loopStartIndex' startIndex 0
@@ -46,7 +48,7 @@ loopStartIndex startIndex machine = loopStartIndex' startIndex 0
           LoopEnd   -> scanLeft $ succ depth
           _         -> scanLeft depth
           where scanLeft = loopStartIndex' (pred index)
-                command  = machine ! index
+                command  = machine !! index
 
 loopEndIndex :: Index -> Machine -> Index
 loopEndIndex startIndex machine = loopEndIndex' startIndex 0
@@ -57,44 +59,48 @@ loopEndIndex startIndex machine = loopEndIndex' startIndex 0
           LoopStart -> scanRight $ succ depth
           _         -> scanRight depth
           where scanRight = loopEndIndex' (succ index)
-                command   = machine ! index
+                command   = machine !! index
 
 main = do
-  path    <- get
-  machine <- lex $ readFile path
-  tape    <- Z.Zip [] [0]
-  stepMachine tape 0 machine
+  path    <- getLine
+  file    <- readFile path
+  machine <- return $ toMachine file
+  tape    <- return $ Z.Zip [] [0]
+  return $ stepMachine tape 0 machine
 
-  where stepMachine tape index mach = case mach ! index of
-    Ignore        -> step tape
+  where
+    stepMachine tape index mach = case mach !! index of
+      Ignore        -> step tape
 
-    TapeLeft      -> step $ if beginp cell
-                             then Z.insert 0 tape
-                             else Z.left tape
+      TapeLeft      -> step $ if Z.beginp tape
+                               then Z.insert 0 tape
+                               else Z.left tape
 
-    TapeRight     -> step $ if endp cell
-                             then Z.insert 0 tape
-                             else Z.right tape
+      TapeRight     -> step $ if Z.endp tape
+                               then Z.insert 0 tape
+                               else Z.right tape
 
-    IncrementCell -> step $ Z.replace (succ cell) tape
-    DecrementCell -> step $ Z.replace (pred cell) tape
+      IncrementCell -> step $ Z.replace (succ cell) tape
+      DecrementCell -> step $ Z.replace (pred cell) tape
 
-    PrintCell     -> do putChar $ chr cell
-                       step tape
+      PrintCell     -> do
+                        putChar (C.chr cell)
+                        step tape
 
-    OverwriteCell -> do putStrLn "Enter a character: "
-                       inChar  <- getChar
-                       step $ Z.replace (ord inChar) tape
+      OverwriteCell -> do
+                        putStrLn "Enter a character: "
+                        inChar <- getChar
+                        step $ Z.replace (C.ord inChar) tape
 
-    LoopStart     -> if cell == 0 -- Loop is over; move past end
-                      then stepMachine tape afterLoopEnd mach
-                      else step tape
+      LoopStart     -> if cell == 0 -- Loop is over; move past end
+                        then stepMachine tape afterLoopEnd mach
+                        else step tape
 
-    LoopEnd       -> if cell == 0 -- Loop is over; move past end
-                      then step tape
-                      else stepMachine tape afterLoopStart mach
+      LoopEnd       -> if cell == 0 -- Loop is over; move past end
+                        then step tape
+                        else stepMachine tape afterLoopStart mach
 
-    where cell = Z.cursor tape
-          step tape' = stepMachine tape' (succ index) mach
-          afterLoopStart = succ loopEndIndex   index mach
-          afterLoopEnd   = succ loopStartIndex index mach
+      where cell = Z.cursor tape :: Int
+            step tape' = stepMachine tape' (succ index) mach
+            afterLoopStart = succ $ loopEndIndex   index mach
+            afterLoopEnd   = succ $ loopStartIndex index mach
