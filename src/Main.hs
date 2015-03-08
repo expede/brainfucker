27929741d6bc10b58afc9eb55252a3af4f181173
Brooklyn Zelenka <be.zelenka@gmail.com>
2015-03-07 20:11:24 -0800

    Works, but very slowly

--- src/Main.hs
+++ src/Main.hs
@@ -21,7 +21,7 @@ data Command = TapeLeft
              | LoopStart
              | LoopEnd
              | Ignore
-             deriving (Show, Eq)
+             deriving (Eq, Show)
 
 charToCommand :: Char -> Command
 charToCommand c = case c of
@@ -41,34 +41,37 @@ toMachine = V.fromList . (filter (/= Ignore)) . (map charToCommand)
 loopStartIndex :: Index -> Machine -> Index
 loopStartIndex startIndex machine = loopStartIndex' startIndex 0
   where loopStartIndex' index depth = case command of
-          LoopStart -> if depth == 0
-                        then index
-                        else scanLeft $ pred depth
+          LoopStart -> case depth of
+                        1 -> index
+                        _ -> scanLeft $ pred depth
           LoopEnd   -> scanLeft $ succ depth
           _         -> scanLeft depth
+
           where scanLeft = loopStartIndex' (pred index)
                 command  = machine V.! index
 
 loopEndIndex :: Index -> Machine -> Index
 loopEndIndex startIndex machine = loopEndIndex' startIndex 0
   where loopEndIndex' index depth = case command of
-          LoopEnd   -> if depth == 0
-                        then index
-                        else scanRight $ pred depth
+          LoopEnd   -> case depth of
+                        1 -> index
+                        _ -> scanRight $ pred depth
           LoopStart -> scanRight $ succ depth
           _         -> scanRight depth
+
           where scanRight = loopEndIndex' (succ index)
                 command   = machine V.! index
 
 main = do
+  putStrLn "Path to input brainfuck file: "
   path    <- getLine
   file    <- readFile path
   machine <- return $ toMachine file
-  tape    <- return $ Z.Zip [] [0]
-  return $ stepMachine tape 0 machine
+  tape'   <- return $ Z.Zip [] [0]
+  stepMachine tape' 0 machine
 
   where
-    stepMachine tape index mach = case mach V.! index of
+    stepMachine tape index mach = runOrEnd $ case mach V.! index of
       Ignore        -> step tape
 
       TapeLeft      -> step $ if Z.beginp tape
@@ -77,29 +80,38 @@ main = do
 
       TapeRight     -> step $ if Z.endp tape
                                then Z.insert 0 tape
-                               else Z.right tape
+                               else Z.insert 0 (Z.right tape)
 
-      IncrementCell -> step $ Z.replace (succ cell) tape
-      DecrementCell -> step $ Z.replace (pred cell) tape
+      IncrementCell -> step $ case cell of
+                        255 -> Z.replace 0 tape
+                        _   -> Z.replace (succ cell) tape
+
+      DecrementCell -> step $ case cell  of
+                        0 -> Z.replace 255 tape
+                        _ -> Z.replace (pred cell) tape
 
       PrintCell     -> do
-                        putChar (C.chr cell)
-                        step tape
+                         putStrLn $ show tape
+                         putChar $ C.chr cell
+                         step tape
 
       OverwriteCell -> do
-                        putStrLn "Enter a character: "
-                        inChar <- getChar
-                        step $ Z.replace (C.ord inChar) tape
+                         putStrLn "Enter a character: "
+                         inChar <- getChar
+                         step $ Z.replace (C.ord inChar) tape
 
-      LoopStart     -> if cell == 0 -- Loop is over; move past end
-                        then stepMachine tape afterLoopEnd mach
-                        else step tape
+      LoopStart     -> case cell of
+                        0 -> stepMachine tape afterLoopEnd mach
+                        _ -> step tape
 
-      LoopEnd       -> if cell == 0 -- Loop is over; move past end
-                        then step tape
-                        else stepMachine tape afterLoopStart mach
+      LoopEnd       -> case cell of
+                        0 -> step tape
+                        _ -> stepMachine tape afterLoopStart mach
 
       where cell = Z.cursor tape :: Int
-            step tape' = stepMachine tape' (succ index) mach
-            afterLoopStart = succ $ loopEndIndex   index mach
-            afterLoopEnd   = succ $ loopStartIndex index mach
+            step tape''    = stepMachine tape'' (succ index) mach
+            afterLoopStart = succ $ loopStartIndex index mach
+            afterLoopEnd   = succ $ loopEndIndex   index mach
+            runOrEnd func  = if index < V.length mach
+                               then func
+                               else putStrLn "0"
